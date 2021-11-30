839ec682c73cff657f157c3f7cd9ddf3d53467a5
Samuel Quaye <quayes955@gmail.com>
2021-11-29 22:19:41 -0500

    user can sign up a user connecting to the users backend

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 const app = angular.module('InventoryApp', []);
 
-app.controller('login', function(){
-    this.label = 'Login: ';
-});
+app.controller('Controller', ['$http', function($http) {
+    console.log('load');
+    this.username = null;
+    this.password = null;
+    // console.log('username', this.username);
+    // console.log('password', this.password);
+
+    this.createUser = function() {
+        // console.log('func called');
+        $http(
+            {
+                method:'POST',
+                url: 'https://inventorial-back.herokuapp.com/api/users',
+                data: {
+                    username: this.username,
+                    password: this.password
+                }
+            }
+        )
+        .then(function(response) {
+            console.log(response);
+        }, function() {
+            console.log('error');
+        });
+    }
+
+    }]
+);
